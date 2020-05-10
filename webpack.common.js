a3185842ac219d6202aca8a4cfc1d33752c7a680
Francis Lagares <francis.lagares@gmail.com>
2020-05-10 18:09:05 +0200

    feat: initial commit
    This commit sets the main starter configuration
    and architecture setup for website development.

---
+++ webpack.common.js
@@ -0,0 +1,74 @@
+const path = require('path')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin')
+
+module.exports = {
+  entry: {
+    main: './src/js/main.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        // from all svg images
+        // we don't want to include sprite image
+        exclude: path.resolve(__dirname, 'src/img/sprite.svg'),
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'img'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        // from all svg images
+        // include only sprite.svg image
+        include: path.resolve(__dirname, 'src/img/sprite.svg'),
+        use: {
+          loader: 'svg-sprite-loader',
+          options: {
+            extract: true,
+            publicPath: '/img/',
+            symbolId: (filePath) => path.basename('')
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'fonts'
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new FaviconsWebpackPlugin({
+      logo: './src/img/favicon.png', // IMPORTANT url has to be set relative if not throws error at yarn build
+      prefix: 'img/',
+      mode: 'light'
+    }),
+    new SpriteLoaderPlugin({
+      plainSprite: true
+    })
+  ]
+}
